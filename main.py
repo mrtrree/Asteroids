3f0b09a7eadfae294aa518d57f16200b98b32540
mrtrree <mrtrree@yahoo.com>
2025-01-22 16:08:57 -0700

    moved updateable and drawable objects to groups in main.py

--- main.py
+++ main.py
@@ -5,19 +5,25 @@ from player import *
 def main():
 
     pygame.init()
-    __clock = pygame.time.Clock()
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    __player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        __clock.tick(60)
-        dt = __clock.tick(60) / 1000
+
+        dt = clock.tick(60) / 1000
         screen.fill((0,0,0))
-        __player.draw(screen)
-        __player.update(dt)
+        for u in updateable:
+            u.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         
 
