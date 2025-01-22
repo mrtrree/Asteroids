ac2b2332dc098dbecbb5a0e8c73d98e764a22228
mrtrree <mrtrree@yahoo.com>
2025-01-22 14:48:08 -0700

    Added circleshape.py to create circle objects

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from constants import *
 
 def main():
     pygame.init()
+    __clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        __clock.tick(60)
+        dt = __clock.tick(60) / 1000
         screen.fill((0,0,0))
         pygame.display.flip()
 
