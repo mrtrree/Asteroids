dda7a5e8d2eec887b4b07da89c8043df045b16fa
mrtrree <mrtrree@yahoo.com>
2025-01-22 16:54:19 -0700

    Added asteroid.py and asteroidfield.py, problem with asteroid.py, looking at draw.circle

---
+++ asteroid.py
@@ -0,0 +1,13 @@
+from circleshape import *
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.radius = radius
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255,255,255), self.radius, width=2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
 
     pygame.init()
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroid = pygame.sprite.Group()
+    Asteroid.containers = (asteroid, updateable, drawable)
     Player.containers = (updateable, drawable)
+    AsteroidField.containers = (updateable)
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    AsteroidField()
 
     while True:
         for event in pygame.event.get():
