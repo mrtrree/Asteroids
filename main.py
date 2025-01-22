c4507f336497cb2346b50185228400e8a128a1aa
mrtrree <mrtrree@yahoo.com>
2025-01-22 15:29:56 -0700

    Added player.py to create player circle shape and put player shape on screen of main.py

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
+
     pygame.init()
     __clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    __player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         __clock.tick(60)
-        dt = __clock.tick(60) / 1000
+        # dt = __clock.tick(60) / 1000
         screen.fill((0,0,0))
+        __player.draw(screen)
         pygame.display.flip()
+        
+
 
 if __name__ == "__main__":
     main()
